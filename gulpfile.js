88bd57300aedfbe61de8ec508b76f001e00df556
hydragit <hydragit@hotmail.com>
2016-06-08 23:02:41 +0100

    set gulp compress images task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
-var gulp    = require('gulp');
-
+//////////////////////////////////////////////////////
+///// Our Gulpfile
+//////////////////////////////////////////////////////
+var gulp     = require('gulp');
+///////////////////////////////////////////////////////
 // include plugins
-var jshint  = require('gulp-jshint');
-var changed = require('gulp-changed');
-var plumber = require('gulp-plumber');
-
-///
-/////////////////////////////////////////////////////
-
-
-
-/// plumber checks for errors in filea
-/// and list's them and prevent gulp from
-/// crashing
-
-/// checked for any js file changed  in source before 
-/// the default task and put's them in ./dist folder
+var jshint   = require('gulp-jshint');
+var changed  = require('gulp-changed');
+var plumber  = require('gulp-plumber');
+var imagemin = require('gulp-imagemin');
+////////////////////////////////////////////////////
+//// Our variables
+////////////////////////////////////////////////////
 var SRC  = './js/*.js';
 var DEST = 'dist'
+///
+/////////////////////////////////////////////////////
+/// plumber checks for errors in file
+/// and list's them and prevent gulp from
+/// crashing
+///////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////
+//// gulp task changed
+/// check for any js file changed  in source before 
+/// the default task and put's them in ./dist folder
 gulp.task('changed', function() {
 	// place code in here
     return gulp.src(SRC)
@@ -27,6 +32,18 @@ gulp.task('changed', function() {
 	.pipe(gulp.dest(DEST));	
 });
 
+//////////////////////////////////////////////////////
+//// gulp task compress-images
+///  compress snd minify images from images folder,
+///  and put's them into our dist img folder "img""
+
+gulp.task('compress-images', function() {
+	// place code in here
+    return gulp.src('pre-images/*')
+	.pipe(imagemin({ optimizationlevel: 7 }))	
+	.pipe(gulp.dest('img'));	
+});
+/////////////////////////////////////////////////////
 /// gulp task jshint
 /// check for errors in js files
 
@@ -38,15 +55,18 @@ gulp.task('jshint', function() {
 	.pipe(jshint.reporter('default'));
 });
 
+////////////////////////////////////////////////////
 /// watch task
 /// looks for any file in SRC changed 
-/// and fires up gulp changed live
+/// and fires up gulp changed live to DEST
 
 gulp.task('watch', function() {
 	// place code in here
     gulp.watch(SRC, ['changed']);	
 });
 
-
+////////////////////////////////////////////////////
 /// Default task of gulp
+///////////////////////////////////////////////////
 gulp.task('default', ['jshint', 'watch']);
+///////////////////////////////////////////////////
