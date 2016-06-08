f747e2e40c4df38fb35b56d4d1d784522d6c115d
hydragit <hydragit@hotmail.com>
2016-06-08 21:39:04 +0100

    set the gulp changed task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp');
+var gulp    = require('gulp');
 
 // include plugins
-var jshint = require('gulp-jshint');
+var jshint  = require('gulp-jshint');
+var changed = require('gulp-changed');
 
+///
+/// checked for any js file changed  in source before 
+/// the default task and put's them in ./dist folder
+var SRC  = './js/*.js';
+var DEST = 'dist'
+
+gulp.task('changed', function() {
+	// place code in here
+    return gulp.src(SRC)
+	.pipe(changed(DEST))
+	.pipe(gulp.dest(DEST));	
+});
 
 /// gulp task jshint
 /// check for errors in js files
